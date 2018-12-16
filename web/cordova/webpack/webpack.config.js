c409d3c0a9b2de0b77f201e652e60b4aab418fef
Memo Cachú <gcachu.o@gmail.com>
2018-12-15 20:54:52 -0600

    webpack

---
+++ web/cordova/webpack/webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: "./src/",
+    output: {
+        path: __dirname + "/../www/assets/dist/"
+    },
+    module: {
+        rules: [
+            {test: /jquery\.js$/, loader: 'expose-loader?jQuery!expose-loader?$'},
+            {test: /\.(gif|png|jpg|svg|cur)$/, loader: 'file-loader?name=img/[name].[ext]'},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})},
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader!sass-loader'})
+            },
+            {test: /\.(eot|woff|ttf|woff2)/, loader: 'file-loader?name=fonts/[name].[ext]'}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("index.css")
+    ]
+};
